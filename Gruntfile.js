369cda21933d32fe0520b4fb47aceb60b6297591
unknown <markocen@gmail.com>
2015-01-09 16:12:38 -0500

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+    options: {
+      separator: ' ',
+    },
+    dist: {
+      src: ['app.js','directive/ngPopupDirective.js','model/ngPopupBuilder.js'],
+      dest: 'dist/ngPopup.js'
+    },
+	},
+	
+	
+  uglify: {
+    options: {
+      mangle: false
+    },
+    my_target: {
+      files: {
+        'dist/ngPopup.min.js': ['dist/ngPopup.js']
+      }
+    }
+  
+}
+});
+  
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat','uglify']);
+
+};
